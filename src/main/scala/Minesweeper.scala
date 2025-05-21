73cd5687bf3a81c408a36be6bdc88a508ceb9dcd
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-21 12:54:10 +0200

    in process

--- src/main/scala/Minesweeper.scala
+++ src/main/scala/Minesweeper.scala
@@ -1,3 +1,10 @@
 object Minesweeper:
    def annotate(l: List[String]): List[String] =
-      l
+      if l.isEmpty || l.size == 1 then l
+      else
+         val grid: List[Array[Char]] = l.map(_.toCharArray)
+         for
+            row <- grid.indices
+            col <- grid.head.indices
+         yield println((row,col))
+         l
