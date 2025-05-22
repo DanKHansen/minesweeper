4278802204fbe4def07dc2ab7060693099698f2d
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-22 11:54:18 +0200

    reformat

--- src/main/scala/Minesweeper.scala
+++ src/main/scala/Minesweeper.scala
@@ -1,13 +1,6 @@
 object Minesweeper:
    def annotate(l: List[String]): List[String] =
-      l.indices.map(r =>
-            l(r).indices.map(c =>
-                  if l(r)(c) == '*' then '*'
-                  else
+      l.indices.map(r => l(r).indices.map(c => if l(r)(c) == '*' then '*' else
                      val n = (-1 to 1).flatMap(dx => (-1 to 1).map(dy => (r + dx, c + dy))).count { case (x, y) =>
-                        (x != r || y != c) && l.indices.contains(x) && l.head.indices.contains(y) && l(x)(
-                          y) == '*'
-                     }
-                     if n == 0 then ' ' else n.toString.head)
-               .mkString)
-         .toList
+                        (x != r || y != c) && l.indices.contains(x) && l.head.indices.contains(y) && l(x)(y) == '*'}
+                     if n == 0 then ' ' else n.toString.head).mkString).toList
